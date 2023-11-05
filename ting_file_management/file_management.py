3fd11c55ba47c4f2be55c50be36299066ea40327
amanda-vlopes <amandavitor@hotmail.com>
2023-11-05 18:13:57 -0300

    Requisito 3 finalizado

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -8,7 +8,7 @@ def txt_importer(path_file):
             return
 
         with open(path_file, 'r') as file:
-            return file.read().splitlines()
+            return file.read().split('\n')
 
     except FileNotFoundError:
         print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+# import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        file = instance.search(index)
+        if file["nome_do_arquivo"] == path_file:
+            return 'Nome do arquivo já existe no sistema'
+
+    linhas_do_arquivo = txt_importer(path_file)
+
+    save_file = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas":  len(linhas_do_arquivo),
+        "linhas_do_arquivo": linhas_do_arquivo
+    }
+
+    instance.enqueue(save_file)
+    print(save_file)
 
 
 def remove(instance):
