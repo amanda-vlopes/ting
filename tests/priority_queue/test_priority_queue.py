06f797387bfb3a02a4f4ef8f3e58345a84d75463
amanda-vlopes <amandavitor@hotmail.com>
2023-11-06 15:37:15 -0300

    Requisito 6 finalizado

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,40 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priority_queue = PriorityQueue()
+    file1 = {
+        "nome_do_arquivo": "arquivo1.txt",
+        "qtd_linhas": 4,
+        "linhas_do_arquivo": ['linha1', 'linha2', 'linha3', 'linha4']
+    }
+    priority_queue.enqueue(file1)
+    assert len(priority_queue) == 1
+
+    file2 = {
+        "nome_do_arquivo": "arquivo2.txt",
+        "qtd_linhas": 7,
+        "linhas_do_arquivo":
+            [
+                'ola',
+                'como vai',
+                'teste',
+                'projeto',
+                'TING',
+                'requisito6',
+                'queue',
+            ]
+    }
+    priority_queue.enqueue(file2)
+    assert len(priority_queue) == 2
+
+    assert priority_queue.is_priority(file1) is True
+    assert priority_queue.is_priority(file2) is False
+
+    assert priority_queue.search(1) == file2
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        priority_queue.search(-1)
+
+    assert priority_queue.dequeue() == file1
