36e24278c5b27db002907a733a66aa742f5d0575
amanda-vlopes <amandavitor@hotmail.com>
2023-11-06 13:56:01 -0300

    Requisito 8 finalizado

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -6,8 +6,7 @@ def exists_word(word, instance):
         ocorrencias = [
             {"linha": i + 1}
             for i, line in enumerate(file["linhas_do_arquivo"])
-            if word.lower() in line.lower()
-        ]
+            if word.lower() in line.lower()]
         if len(ocorrencias) > 0:
             files.append({
                 "palavra": word,
@@ -17,26 +16,18 @@ def exists_word(word, instance):
     return files
 
 
-instance = [
-    {
-        "nome_do_arquivo": "arquivo_teste.txt",
-        "qtd_linhas": 3,
-        "linhas_do_arquivo": ['hello', 'henrique', 'how are you?']
-    },
-    {
-        "nome_do_arquivo": "arquivo_teste2.txt",
-        "qtd_linhas": 3,
-        "linhas_do_arquivo": ['hi', 'amanda', 'how are you?']
-    },
-    {
-        "nome_do_arquivo": "arquivo_teste3.txt",
-        "qtd_linhas": 4,
-        "linhas_do_arquivo": ['hey', 'you', 'how are you?', 'he is fine']
-    }
-]
-
-# print(exists_word('he', instance))
-
-
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    files = list()
+    for index in range(len(instance)):
+        ocorrencias = list()
+        file = instance.search(index)
+        for i, line in enumerate(file["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                ocorrencias.append({"linha": i + 1, "conteudo": line})
+        if len(ocorrencias) > 0:
+            files.append({
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": ocorrencias
+            })
+    return files
